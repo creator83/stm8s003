bf180bff769d840182795cee03634d3ff66c3570
Home_desktop <creator83@yandex.ru>
2015-10-29 23:01:06 +0300

    29_10_15

--- c/prj/use_gpio.c
+++ c/prj/use_gpio.c
@@ -1,19 +1,20 @@
 #include "stm8s.h"
 #include "delay.h"
 
-#define led 1
+#define led 4
+#define PORT GPIOB
 
 int main( void )
 {
   CLK->CKDIVR &= ~CLK_CKDIVR_HSIDIV;
   CLK->CKDIVR &= ~CLK_CKDIVR_CPUDIV;
-  GPIOA->DDR |= 1 << led;
-  GPIOA->CR1 |= 1 << led;
+  PORT->DDR |= 1 << led;
+  PORT->CR1 |= 1 << led;
   
   
   while (1)
   {
-    GPIOA->ODR ^= 1 << led;
+    PORT->ODR ^= 1 << led;
     delay_ms (1000);
   }
 }
