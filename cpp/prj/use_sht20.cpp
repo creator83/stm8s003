9fadd5953e53349e76d13ca566b176fdc569a2db
Work_desktop <creator83@yandex.ru>
2017-02-03 15:30:23 +0300

    sd

--- cpp/prj/use_sht20.cpp
+++ cpp/prj/use_sht20.cpp
@@ -17,29 +17,39 @@
 #include "softi2c.h"
 
 Tact frq;
-Buffer value;
-uint8_t *eepromPtr;
-SoftI2c driver (Gpio::B , 5, Gpio::B, 4);
-//SoftI2c driver;
-Sht20 sensor (&driver);
+
 
 void initAwu ();
 
 int main()
 { 
-  Sht20 sensor (&driver);
  // Nrf24l01 ();
- CLK->PCKENR1 = 1 << 1;
- CLK->PCKENR2 = 1 << 2;
-  
+  /*CLK->PCKENR1 = 1 << 1;
+  CLK->PCKENR2 = 1 << 2;*/
+  CLK->PCKENR1 = 0;
+  CLK->PCKENR2 = 0;
+  GPIOA->DDR = 0X00;
+  GPIOA->CR1 = 0XFF;
+  GPIOA->CR2 = 0X00;
+  GPIOB->DDR = 0X00;
+  GPIOB->CR1 = 0XFF;
+  GPIOB->CR2 = 0X00;
+  GPIOC->DDR = 0X00;
+  GPIOC->CR1 = 0XFF;
+  GPIOC->CR2 = 0X00;
+  GPIOD->DDR = 0X00;
+  GPIOD->CR1 = 0XFF;
+  GPIOD->CR2 = 0X00;
   frq.init_lsi();
+  SoftI2c driver (Gpio::B , 5, Gpio::B, 4);
+//SoftI2c driver;
+  Sht20 sensor (&driver);
   /*initAwu ();
   halt ();*/
  //frq.setHsiFrq(3);
   while (1)
   {
-   // CLK->PCKENR1 &= ~ 1 << 5;
-   
+   // CLK->PCKENR1 &= ~ 1 << 5; 
     sensor.getHummidity ();
     sensor.getTemperature ();
   //  CLK->PCKENR1 = 1 << 5;
