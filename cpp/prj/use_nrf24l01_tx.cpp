b36af81dd16cfba99bc16bedffef574abe7c271c
creator83 <creator83@yandex.ru>
2018-02-28 21:35:32 +0300

    d

--- cpp/prj/use_nrf24l01_tx.cpp
+++ cpp/prj/use_nrf24l01_tx.cpp
@@ -9,20 +9,12 @@
 Tact frq;
 Uart uart1 (Uart::b9600);
 Nrf24l01 radio;
-const uint8_t led = 3;
+//const uint8_t led = 3;
 Gpio D (Gpio::D);
 
 
 INTERRUPT_HANDLER(EXTA_i, EXTI0_vector)
 {
-  uint8_t status = radio.get_status ();
-  uart1.transmit (status);
-  radio.w_reg (STATUS, status);
-  status = radio.get_status ();
-  uart1.transmit (status);
-  D.setPin (led);
-  delay_ms (1500);
-  D.clearPin (led);
 }
 
 
@@ -30,8 +22,6 @@ INTERRUPT_HANDLER(EXTA_i, EXTI0_vector)
 
 int main()
 {
-  D.setOutPin (led);
-  radio.send_byte (125);
   while (1)
   {  
     /*for (uint8_t i=0;i<100;++i)
